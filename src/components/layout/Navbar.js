0c85e4546a29ace30e10f93463727f543eeac969
Can <mca199667@gmail.com>
2020-09-23 18:31:59 +0300

    Navbar menu button and nav list on media query

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [toggled, setToggled] = useState(false);
+
   return (
     <nav className="navbar">
       <Link to="/" className="nav-logo">
         <img src={require("../../assets/img/logo-light.png")} alt="App logo" />
       </Link>
-      <ul className="nav-list">
+      <ul className={toggled ? "nav-list toggled" : "nav-list"}>
         <li className="nav-link">
           <Link to="/">
             <i className="fas fa-home"></i>
@@ -20,6 +22,14 @@ const Navbar = () => {
           <Link to="/about">About</Link>
         </li>
       </ul>
+      <button
+        className={toggled ? "nav-menu-btn toggled" : "nav-menu-btn"}
+        onClick={() => setToggled(!toggled)}
+      >
+        <span className="top"></span>
+        <span className="middle"></span>
+        <span className="bottom"></span>
+      </button>
     </nav>
   );
 };
