fbbad07025f305a6335655eb8c7537f6af10d664
Can <mca199667@gmail.com>
2020-09-24 18:57:41 +0300

    Generate button styling

--- src/components/pages/Generate.js
+++ src/components/pages/Generate.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Generate = () => {
-  return <main>Generate</main>;
+  return (
+    <main className="page generate-page container">
+      <button className="generate-btn">
+        <span className="outer"></span>
+        <span className="inner">Go</span>
+      </button>
+      <div className="result">
+        <h2>Title</h2>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+          <li>c</li>
+          <li>d</li>
+          <li>e</li>
+        </ul>
+      </div>
+    </main>
+  );
 };
 
 export default Generate;
