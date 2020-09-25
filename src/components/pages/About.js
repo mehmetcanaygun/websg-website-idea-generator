01a5ceaf7157644d730b4a955b86657bf00b54dc
Can <mca199667@gmail.com>
2020-09-25 16:36:03 +0300

    About page design

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,179 @@
 import React from "react";
 
 const About = () => {
-  return <main>About</main>;
+  return (
+    <main className="page about-page container">
+      <h2>About</h2>
+      <p>
+        This website is created by{" "}
+        <a
+          href="https://mehmetcanaygun.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mehmet Can Aygün
+        </a>{" "}
+        for web developers who want to improve their skills by creating
+        websites. Practicing is the best way to learn. Keep building!
+      </p>
+      <h2>Useful Resources</h2>
+      <ul>
+        <li>
+          <a
+            href="https://fonts.google.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Google Fonts
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://fontawesome.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Font Awesome
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://unsplash.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Unsplash
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.pexels.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Pexels
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.videvo.net/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Videvo
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://picsum.photos/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lorem Picsum
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.drawkit.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Drawkit
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://undraw.co/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            unDraw
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.cssmatic.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CSS matic
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://cssgradient.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CSS Gradient
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://stripesgenerator.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CSS Stripes Generator
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://cubic-bezier.com/#.17,.67,.83,.67"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            cubic-bezier
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://coolors.co/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Coolors
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://getwaves.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Get Waves
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.heropatterns.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hero Patterns
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://michalsnik.github.io/aos/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            AOS Animate On Scroll Library
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/bradtraversy/design-resources-for-developers"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Brad Traversy - Design Resources For Developers
+          </a>
+        </li>
+      </ul>
+    </main>
+  );
 };
 
 export default About;
